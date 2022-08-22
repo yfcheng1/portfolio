79535b97625f45b00b9aa7682746ee1bbd10c1ba
yfcheng1 <b99501048@gmail.com>
2022-08-22 00:03:39 -0400

    add profolio project

--- portfolio/src/components/Layout.js
+++ portfolio/src/components/Layout.js
@@ -6,13 +6,13 @@ const Layout = () => {
     <div className="Layout">
       <Sidebar/>
       <div className="page">
-        <span className="tags top-tags">&lt;body&gt;</span>
+        <span className="tags top-tags"></span>
 
         <Outlet />
         <span className="tags bottom-tags">
-          &lt;/body&gt;
+          
           <br />
-          <span className="bottom-tag-html">&lt;/html&gt;</span>
+          <span className="bottom-tag-html"></span>
         </span>
       </div>
     </div>
--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,6 +1,10 @@
 import { useState } from 'react';
 import Loader from 'react-loaders';
 import AnimatedLetters from './AnimatedLetters';
+import payment1 from '../assets/payment1.gif';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons';
 
 const Project = () => {
   const [letterClass] = useState('text-animate');
@@ -16,28 +20,34 @@ const Project = () => {
               idx={15}
             />
           </h1>
-          <img className="project-img" src="" alt="screenshot of Splitsy project"/>
-          <div className="project-info">
-            <div className="project-link-block row">
-              <a className="project-website" href="https://github.com/rpp33-boc-arctic/splitsy" aria-label="link to github repo">
-                <i className="fa fab fa-github link"></i>
-              </a>
-              <a className="project-website" href="https://github.com/rpp33-boc-arctic/splitsy#main-features" aria-label="link to live page">
-                <i className="fa fas fa-external-link-alt link"></i>
-              </a>
-            </div>
-            <a className="project-title" href="https://github.com/rpp33-boc-arctic/splitsy" rel="noopener noreferrer" target="_blank">Splitsy</a>
-            <div className="project-desc">
-              A full stack web application that allows users to conveniently split restaurant bills! Create or join an order easily
-              and share with friends and family, share the bill and tips with different methods, then submit the order to the restaurant.
-            </div>
-            <div className="project-tech-block">
-              <span className="project-tech">React</span>
-              <span className="project-tech">MaterialUI</span>
-              <span className="project-tech">Express</span>
-              <span className="project-tech">MongoDB</span>
-              <span className="project-tech">Paypal SDK</span>
-            </div>
+          <img className="project-img" src={payment1} alt="screenshot of Splitsy project"/>
+        </div>
+        <div className="project-info">
+          <a className="project-title" href="https://github.com/rpp33-boc-arctic/splitsy" rel="noopener noreferrer" target="_blank">Splitsy</a>
+          <div className="project-link-block">
+            <a
+              className="project-website"
+              href="https://github.com/rpp33-boc-arctic/splitsy"
+              aria-label="link to github repo">
+              <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+            </a>
+            <a
+              className="project-website"
+              href="https://github.com/rpp33-boc-arctic/splitsy#main-features"
+              aria-label="link to live page">
+              <FontAwesomeIcon icon={faCode} color="#4d4d4e" />
+            </a>
+          </div>
+          <div className="project-desc">
+            A full stack web application that allows users to conveniently split restaurant bills! Create or join an order easily
+            and share with friends and family, share the bill and tips with different methods, then submit the order to the restaurant.
+          </div>
+          <div className="project-tech-block">
+            <span className="project-tech">React</span>
+            <span className="project-tech">MaterialUI</span>
+            <span className="project-tech">Express</span>
+            <span className="project-tech">MongoDB</span>
+            <span className="project-tech">Paypal SDK</span>
           </div>
         </div>
       </div>
